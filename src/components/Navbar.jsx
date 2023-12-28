fe24d2c0b4f7f09ba52e11f52be6d99c81551753
isakibul <isakibul108@gmail.com>
2023-12-28 17:05:28 +0600

    project initilize and navbar added

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,68 @@
+import React, { useState } from "react";
+import NavbarElements from "./NavbarElements";
+import { ImMenu } from "react-icons/im";
+import { IoClose } from "react-icons/io5";
+
+const Navbar = () => {
+    const [toggle, setToggle] = useState(false);
+
+    const handleToggle = () => {
+        setToggle(!toggle);
+    }
+
+    console.log(toggle);
+    return (
+        <div className="px-10 md:px-30 lg:px-40">
+            <div className="flex justify-between my-6 items-center">
+                <div>
+                    <a href="/">
+                        <img src="/logo.png" alt="site-log" className="w-10" />
+                    </a>
+                </div>
+                <div>
+                    <ul className="hidden md:flex gap-10 text-2xl">
+                        {NavbarElements.map((item, idx) => (
+                            <li key={idx}>
+                                <a href={item.path}>
+                                    {item.name}
+                                </a>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+                <div>
+                    <button
+                        className="hidden md:block bg-blue-600 hover:bg-blue-800 px-4 py-2 text-white text-xl font-bold rounded"
+                    >
+                        Resume
+                    </button>
+                </div>
+                <div className="md:hidden">
+                    {toggle ? <IoClose fontSize={40} cursor={"pointer"} onClick={handleToggle} /> : <ImMenu fontSize={40} cursor={"pointer"} onClick={handleToggle} />}
+                </div>
+            </div>
+
+            {/* mobile menu */}
+            {toggle && (
+                <div className="md:hidden text-2xl text-center flex flex-col">
+                    <ul>
+                        {NavbarElements.map((item, idx) => (
+                            <li key={idx} className="mb-4">
+                                <a href={item.path}>
+                                    {item.name}
+                                </a>
+                            </li>
+                        ))}
+                    </ul>
+                    <button
+                        className="bg-blue-600 hover:bg-blue-800 px-4 py-2 text-white text-xl font-bold rounded"
+                    >
+                        Resume
+                    </button>
+                </div>
+            )}
+        </div>
+    );
+};
+
+export default Navbar;
---
+++ src/components/NavbarElements.js
@@ -0,0 +1,16 @@
+const NavbarElements = [
+  {
+    name: "Projects",
+    path: "https://www.google.com/",
+  },
+  {
+    name: "About",
+    path: "https://www.google.com/",
+  },
+  {
+    name: "Contact",
+    path: "https://www.google.com/",
+  },
+];
+
+export default NavbarElements;
