e0dedf413c999846cb9d4c64cc94cdfe873ef31f
isakibul <isakibul108@gmail.com>
2024-01-12 15:18:39 +0600

    project updated

--- src/components/Projects/ProjectData.js
+++ src/components/Projects/ProjectData.js
@@ -1,14 +1,23 @@
-import projectImg from "/project-photo.png";
+import nextverse from "/nextverse.png";
+import hungryhulk from "/hungryhulk.png";
 
 const projectData = [
   {
-    image: projectImg,
+    image: nextverse,
     title: "NextVerse",
     description:
       "Build an elegant social media platform motivated by Instagram.",
-    technologies: ["React", "Firebase"],
+    technologies: ["React", "Chakra UI", "Firebase"],
     github: "https://github.com/isakibul/nextverse",
     demo: "https://nextverse-zeta.vercel.app/",
+  },
+  {
+    image: hungryhulk,
+    title: "HungryHulk",
+    description: "A stunning restaurant application with plenty of context.",
+    technologies: ["React", "Tailwind CSS", "Strapi"],
+    github: "https://github.com/isakibul/hungryhulk",
+    demo: "https://hungryhulk.vercel.app/",
   },
 ];
 
